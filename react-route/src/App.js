e644f63d50d546365d80fecc730fb290c8ad86eb
Dev-Ganesh-Sable <dev.sableganesh0612@gmail.com>
2026-04-21 21:25:07 +0530

    add navbar component

--- react-route/src/App.js
+++ react-route/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ganesh</h1>
+      <Navbar />
     </div>
   );
 }
